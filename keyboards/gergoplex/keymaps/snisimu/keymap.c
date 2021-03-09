75bb8ad32bf11341a65c40d10bfcf6e21d8ab3b3
Shinji Nishimura <snisimu@gmail.com>
2021-03-09 21:50:43 +0900

    (work in progress - by ShutdownEtc.ps1 snisi@HOMENUC)

--- keyboards/gergoplex/keymaps/snisimu/keymap.c
+++ keyboards/gergoplex/keymaps/snisimu/keymap.c
@@ -8,10 +8,10 @@
 
 #include QMK_KEYBOARD_H
 
-#define BASE 0 // default layer
+#define BASE 0   // default layer
 #define NUMSYM 1 // number/symbol layer
 #define CURSOR 2 // cursor layer
-#define FUNC   3 // function layer
+#define FUNCTI 3 // function layer
 
 // Combos
 enum combos {
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_BSLASH, KC_SLASH, KC_EQUAL, KC_MINUS, KC_NO, /* */ KC_NO, KC_GRAVE, KC_TILDE, KC_LBRACKET, KC_RBRACKET,
   KC_1, KC_2, KC_3, KC_4, KC_5, /* */ KC_6, KC_7, KC_8, KC_9, KC_0,
   KC_F1, KC_F2,  KC_LALT,  KC_LCTRL,  KC_F5, /* */ KC_F6, KC_RCTRL, KC_RALT, KC_F9, KC_F10,
-  MO(FUNC), KC_LGUI, KC_LSHIFT, // Left
+  MO(FUNCTI), KC_LGUI, KC_LSHIFT, // Left
   KC_NO, KC_TRANSPARENT, KC_NO // Right
 ),
 
@@ -50,12 +50,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_NO, KC_RSHIFT, KC_ENTER // Right
 ),
 
-[FUNC] = LAYOUT_gergoplex(
+[FUNCTI] = LAYOUT_gergoplex(
   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
   KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, /* */ KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, 
   KC_F1, KC_F2,  KC_F3,  KC_F4,  KC_F5, /* */ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
   KC_TRANSPARENT, KC_LGUI, KC_LSHIFT, // Left
   KC_NO, KC_TRANSPARENT, KC_NO // Right
-),
+)
 
 };
